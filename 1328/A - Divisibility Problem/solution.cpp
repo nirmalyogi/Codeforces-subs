afe33c1361d62f19de8dda59d4d5d1101774c191
BeginnerThesis <nirmalyogi4u@gmail.com>
2026-08-14 21:12:31 +0530

    Add Divisibility Problem [A] from Codeforces

---
+++ 1328/A - Divisibility Problem/solution.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+ 
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+        int a, b;
+        cin >> a >> b;
+ 
+        if (a % b == 0) {
+            cout << 0 << "
+";
+        } else {
+            cout << b - (a % b) << "
+";
+        }
+    }
+    return 0;
+}
